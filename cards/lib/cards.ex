b6c8029e2f3305ee3470501ddd4a459bb165b5ae
Chris Anderson <cl.anderson8@gmail.com>
2016-12-01 13:52:19 -0600

    over comments and :erlang stuff

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -25,6 +25,7 @@ defmodule Cards do
   end
 
   def contains?(deck, card) do
+    # if function returns a bool, then convention is to name it with 
     Enum.member?(deck, card)
   end
 
@@ -33,5 +34,23 @@ defmodule Cards do
     #this will create a tuple where the first value will be the hand, and the second val will be the remaining deck.
     #this key value pair, but without the keys... the location of the object (first pos, second pos) will determine
     #what the values mean
+  end
+
+  def save(deck, filename) do
+    #whenever we need to write Erlang code under elixir, we write the :erlang symbol
+    #this code is using :erlang's built in term_to_binary function
+    #which we're going save using the Elixir File module.
+    binary = :erlang.term_to_binary(deck)
+    File.write(filename, binary) 
+  end
+
+  def load(filename) do
+    {status, binary} = File.read(filename)
+    #Instructor note: Try to avoid if statements at all possible cost. Try to use
+    #case statements like the one below and pattern matching
+    case status do
+      :ok -> :erlang.binary_to_term binary
+      :error -> "that file doesn't exist"
+    end
   end
 end
