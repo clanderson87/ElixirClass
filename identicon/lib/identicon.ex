1ff2e6e99f3f8cfc8da17756a5f005f8556e2ea3
Chris Anderson <cl.anderson8@gmail.com>
2016-12-05 16:33:57 -0600

    pattern matching in method definition

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -5,12 +5,16 @@ defmodule Identicon do
     |> pick_color
   end
 
-  def pick_color(image) do
-    %Identicon.Image{hex: [r, g, b | _tail]} = image
+  def pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
+    #Above is an example of pattern matching in the method definition. Neat.
     #the pipe _tail tells elixir that we acknoledge that hex_list has more than three values,
     # but remember, with the _var, we just don't care about anything after the pipe!
-    [r, g, b]
+    %Identicon.Image{image | color: {r, g, b}}
+    #Here we're creating a new struct that consumes the image passed into pick_color/1
+    #In functional programming, there is no image.color = rgb... there is just data.
   end
+
+  
 
   def hash_input(input) do
     hexVal = :crypto.hash(:md5, input)
--- identicon/lib/image.ex
+++ identicon/lib/image.ex
@@ -3,7 +3,7 @@ defmodule Identicon.Image do
   #structs state that the only data that a struct can hold is what we define.
   #In this case, a struct can only hold a hex property. Maps, on the other hand,
   #data floosies that let you hold any data you want in there.
-  defstruct hex: nil
+  defstruct hex: nil, color: nil
   #Remember, this is a primitive data type... not a method container,
   #nor a model.
 end
