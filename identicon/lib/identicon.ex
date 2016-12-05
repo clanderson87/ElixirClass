66ce686f47a2a3840d3f37396f77f79d9f4e60d2
Chris Anderson <cl.anderson8@gmail.com>
2016-12-05 16:26:14 -0600

    Lesson 40

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -2,6 +2,14 @@ defmodule Identicon do
   def main(input) do
     input
     |> hash_input
+    |> pick_color
+  end
+
+  def pick_color(image) do
+    %Identicon.Image{hex: [r, g, b | _tail]} = image
+    #the pipe _tail tells elixir that we acknoledge that hex_list has more than three values,
+    # but remember, with the _var, we just don't care about anything after the pipe!
+    [r, g, b]
   end
 
   def hash_input(input) do
