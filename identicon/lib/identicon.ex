20cd46f309e5fbe6c6a0e1a30ab958ba7cbb37f8
Chris Anderson <cl.anderson8@gmail.com>
2016-12-05 16:41:34 -0600

    ham fisted mirror row function

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -3,6 +3,20 @@ defmodule Identicon do
     input
     |> hash_input
     |> pick_color
+    |> build grid
+  end
+
+  def build_grid(%Identicon.Image{hex: hex} = image) do
+    hex
+    |> Enum.chunk(3)
+    |> 
+  end
+
+  def mirror_row(row) do
+    # -> [145, 46, 20]
+    [first, second | _tail] = row
+    # [145, 46, 200, 46, 145] ->
+    row ++ [second, first]
   end
 
   def pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
@@ -14,7 +28,7 @@ defmodule Identicon do
     #In functional programming, there is no image.color = rgb... there is just data.
   end
 
-  
+
 
   def hash_input(input) do
     hexVal = :crypto.hash(:md5, input)
