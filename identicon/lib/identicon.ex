859370d78ec4edceed051cbf05ff82c1eacc317f
Chris Anderson <cl.anderson8@gmail.com>
2016-12-05 17:24:39 -0600

    fixed dumb typos. Demo .png. Merging.

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -3,7 +3,7 @@ defmodule Identicon do
     input
     |> hash_input
     |> pick_color
-    |> build grid
+    |> build_grid
     |> filter_odd_squares
     |> build_pixel_map
     |> draw_image
@@ -20,7 +20,7 @@ defmodule Identicon do
     image = :egd.create(250, 250)
     fill = :egd.color(color)
 
-    Enum.each pixel_map fn({start, stop}) ->
+    Enum.each pixel_map, fn({start, stop}) ->
       :egd.filledRectangle(image, start, stop, fill)
     end
 
@@ -28,7 +28,7 @@ defmodule Identicon do
   end
 
   def build_pixel_map(%Identicon.Image{grid: grid} = image) do
-    pixel_map = Enum.map grid, fn({_code, index}) ->
+    pixel_mapVal = Enum.map grid, fn({_code, index}) ->
       horizontal = rem(index, 5) * 50
       vertical = div(index, 5) * 50
       # this creates the point to start the square from. This is the top left corner.
@@ -39,7 +39,7 @@ defmodule Identicon do
       {top_left, bottom_right}
     end
 
-    %Identicon.Image{image | pixel_map}
+    %Identicon.Image{image | pixel_map: pixel_mapVal}
   end
 
   def filter_odd_squares(%Identicon.Image{grid: grid} = image) do
