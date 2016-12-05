8b0bbab38b1e790da237804ac2fc82a77da4476c
Chris Anderson <cl.anderson8@gmail.com>
2016-12-05 16:54:56 -0600

    build grid done

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -4,13 +4,19 @@ defmodule Identicon do
     |> hash_input
     |> pick_color
     |> build grid
+    #due to piping, every method in this flow must return the same datatype - in this case, a %Identicon.Image{}
   end
 
   def build_grid(%Identicon.Image{hex: hex} = image) do
-    hex
-    |> Enum.chunk(3)
-    |> Enum.map(&mirror_row/1)
-    #the &mirror_row/1 is how we pass function refs in Elixir. &function_name/arity#
+    gridVal = 
+      hex
+      |> Enum.chunk(3)
+      |> Enum.map(&mirror_row/1) #the &mirror_row/1 is how we pass function refs in Elixir. &function_name/arity#
+      |> List.flatten #makes a list of lists into one list of values (that were in the other list)
+      |> Enum.with_index #Since elixir is too cool for indices, this takes ever element in a list and makes it a tuple: [{value01, 0}, {value2, 1}, etc...]
+
+    %Identicon.Image{image | grid: gridVal}
+    #take everything from the image piped in, consume it, and create a new image struct with this additional value declared after the pipe
   end
 
   def mirror_row(row) do
