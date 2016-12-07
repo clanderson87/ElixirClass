f6b1906b60fccefb2e5466596f33bb58af2bda29
Chris Anderson <cl.anderson8@gmail.com>
2016-12-07 14:47:01 -0600

    comments in understanding changeset

--- phoenix/discuss/web/models/topic.ex
+++ phoenix/discuss/web/models/topic.ex
@@ -9,8 +9,12 @@ defmodule Discuss.Topic do
   def changeset(struct, params \\ %{}) do
     #struct represents a record in our database
     #params represents how we want to update the struct with
+    #the \\%{} is a default argument that we're passing to params.
+    #if params is blank, it defaults to nil
     struct
     |> cast(params, [:title])
+    #returns an Ecto.Changeset datatype - not a 
+    # changeset function like the one we're in - which 
     #describes how we want to update the database
     |> validate_required([:title])
     #this will check if the model is valid or not
