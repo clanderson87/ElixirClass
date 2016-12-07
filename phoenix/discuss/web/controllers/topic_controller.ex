cdaa66f957d21d91a96183666556cc9cf1ac041a
Chris Anderson <cl.anderson8@gmail.com>
2016-12-07 14:56:54 -0600

    I just made a template. I think.

---
+++ phoenix/discuss/web/controllers/topic_controller.ex
@@ -0,0 +1,18 @@
+defmodule Discuss.TopicController do
+  use Discuss.Web, :controller
+  alias Discuss.Topic
+  # like class inheritance. Imports a ton of stuff from Discuss.Web.ex/controller
+
+  def new(conn, params) do
+    # IO.puts "++++"
+    # IO.inspect conn #The focus of Phoenix. CONN IS AWESOME
+    # IO.puts "++++"
+    # IO.inspect params #Help us parse the URL.
+    # IO.puts "++++"
+
+    struct = %Topic{}
+    params = %{}
+    changeset = Topic.changeset(struct, params)
+    render conn, "new.html"
+  end
+end
