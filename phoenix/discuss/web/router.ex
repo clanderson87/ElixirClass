a2f6527b8439a490d3add2d32b4fd4fa4ac671a8
Chris Anderson <cl.anderson8@gmail.com>
2016-12-08 15:51:30 -0600

    Deleting Topics

--- phoenix/discuss/web/router.ex
+++ phoenix/discuss/web/router.ex
@@ -22,12 +22,16 @@ defmodule Discuss.Router do
     #   to the "/" route
     #   find to the TopicController module
     #   and run the index method
-    get "/topics/new", TopicController, :new
-    post "/topics", TopicController, :create
-    get "/topic/:id/edit", TopicController, :edit
+    # get "/topics/new", TopicController, :new
+    # post "/topics", TopicController, :create
+    # get "/topic/:id/edit", TopicController, :edit
     # :id is a wildcard matcher. it needs to match the text passed into the 
     # params map in the corresponding controller method (in this case, :edit)
-    put "/topics/:id", TopicController, :update
+    # put "/topics/:id", TopicController, :update
+    # delete "/topics/:id", TopicController, :delete
+
+    resources "/", TopicController #if restful conventions are followed, this automates all the routes for this controller
+    # based upon the type of request and info that has been provided
   end
 
   # Other scopes may use custom stacks.
