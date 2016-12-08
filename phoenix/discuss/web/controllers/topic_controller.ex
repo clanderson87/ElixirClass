a0724936ff689e0c3ec020e6683656791eb27d92
Chris Anderson <cl.anderson8@gmail.com>
2016-12-08 14:13:48 -0600

    Materialize iocns and Elixir <a> tags

--- phoenix/discuss/web/controllers/topic_controller.ex
+++ phoenix/discuss/web/controllers/topic_controller.ex
@@ -18,7 +18,7 @@ defmodule Discuss.TopicController do
 
   def index(conn, _params) do
     topics = Repo.all(Topic) #note here that were passing in a module - this will return all the data records of this module type
-    render conn, "index.html", topics: topics
+    render conn, "index.html", topics: topics #this makes the @topics property availible to index.html.eex
   end
 
   def create(conn, params) do
@@ -28,7 +28,11 @@ defmodule Discuss.TopicController do
     changeset = Topic.changeset(%Topic{}, topic)
 
     case Repo.insert(changeset) do
-      {:ok, post} -> IO.inspect(post)
+      {:ok, post} -> 
+        IO.inspect(post)
+        conn
+        |> put_flash(:info, "topic created") #shows single use messages to the user, destroyed upon next user action shown. shown in templates/layout/app.html.eex:26-27
+        |> redirect(to: topic_path(conn, :index))
       {:error, changeset} -> 
         IO.inspect(changeset)
         render conn, "new.html", changeset: changeset
