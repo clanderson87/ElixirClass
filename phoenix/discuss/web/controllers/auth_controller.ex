01df387135b4130fc79be5a6a361206df9c4b315
Chris Anderson <cl.anderson8@gmail.com>
2016-12-13 19:30:42 -0600

    github auth working

--- phoenix/discuss/web/controllers/auth_controller.ex
+++ phoenix/discuss/web/controllers/auth_controller.ex
@@ -5,15 +5,34 @@ defmodule Discuss.AuthController do
   
   def callback(%{assigns: %{ueberauth_auth: auth}} = conn, params) do
     user_params = %{token: auth.credentials.token, email: auth.info.email, provider: "github"}
+    # taken from the conn object - use IO.inspect(conn) to see this
     changeset = User.changeset(%User{}, user_params)
 
-    insert_or_update_user(changeset)
+    signin(conn, changeset)
+  end
+
+  defp signin(conn, changeset) do
+    case insert_or_update_user(changeset) do
+      {:ok, user} ->
+        conn
+        |> put_flash(:info, "Welcome back!")
+        |> put_session(:user_id, user.id)
+        |>redirect(to: topic_path(conn, :index))
+
+      {:error, _reason} ->
+        conn
+        |> put_flash(:error, "error signing in")
+        |> redirect(to: topic_path(conn, :index))
+    end # defined below
   end
 
   defp insert_or_update_user(changeset) do
     # defp is a private function 
+    IO.inspect(changeset)
     case Repo.get_by(User, email: changeset.changes.email) do
+      #add a clause for when email is private on Github here for {:error}
       nil ->
+        #if this returns nothing
         Repo.insert(changeset)
       user ->
         {:ok, user}
